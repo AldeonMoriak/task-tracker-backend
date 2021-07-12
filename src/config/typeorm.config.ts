128677d179795bf6ea30ce007eb7210ba456d277
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-07-12 23:02:39 +0430

    change config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,16 +6,16 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 54321,
-  username: 'aldeon',
-  password: 'password',
-  database: 'tasktracker',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 54321,
+  username: process.env.DB_USER || 'aldeon',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'tasktracker',
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   extra: { charset: 'utf8mb4_unicode_ci' },
   // TODO: synchronize must be false in production phase
   synchronize: process.env.NODE_ENV !== 'production',
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
