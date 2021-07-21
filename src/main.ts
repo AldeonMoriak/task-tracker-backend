28e866cf5d44bf0386837f52065ff0a808c5bf7f
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-07-21 20:02:54 +0430

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: false,
+    origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     preflightContinue: false,
     optionsSuccessStatus: 204,
